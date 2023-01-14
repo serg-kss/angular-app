f487741d76b34ddaaca5d6763adfe64d4e5e4ccc
Serg <korobko_ss@ukr.net>
2023-01-14 12:47:26 +0200

    admin/orders

--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -9,11 +9,21 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class OrdersComponent {
   orders: Delivery[] = [];
+  orders_amount: number;
+  total_amount_EUR: number;
 
   constructor(public productService: ProductService) {
     this.productService.getAllOrders().subscribe((response) => {
       this.orders = response;
+      this.orders_amount = response.length;
+
+      let i = 0;
+      for (let index = 0; index < response.length; index++) {
+        const element = parseFloat(response[index].amount);
+        i = i + element;        
+      }
+
+      this.total_amount_EUR = i;
     });
   }
-
 }
